0bff6ab8db5022fa3a4a23838a8414bc25551957
shunak <shuna966@gmail.com>
2020-09-28 15:05:11 +0900

    modify

--- pipeline.sh
+++ pipeline.sh
@@ -1,6 +1,11 @@
+# label sentence and outputs to __label__x.txt
 python3 preprocess1.py claim.txt
 python3 preprocess2.py prefer.txt
+
+# unify labeled files
 cat __label__1.txt __label__2.txt > model.txt
+
+# make model file
 python3 learning.py model.txt model
 
 
