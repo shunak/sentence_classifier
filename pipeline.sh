6afeee9a98ac5d0a315d45143b327e53f5085b69
shunak <shuna966@gmail.com>
2020-09-28 15:25:45 +0900

    modify

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,6 @@
 # label sentence and outputs to __label__x.txt
 # set text file name which you wanna labeling xxx.txt
+# if you classify sentence as claim or prefer for example, set claim.txt , prefer.txt like underneath
 python3 preprocess1.py claim.txt
 python3 preprocess2.py prefer.txt
 
